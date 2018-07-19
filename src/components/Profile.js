e91fb243ca884177cd46a8d8aeeddd1443db1418
Ali <ali@Alis-iMac.fritz.box>
2018-07-19 17:12:35 +0200

    error handeling for profile page added, plus terms and conditions

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, SafeAreaView, View, Button } from 'react-native'
+import { Text, SafeAreaView, View, Button, ActivityIndicator, Linking, Switch } from 'react-native'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
@@ -18,16 +18,32 @@ const deviceNameSafe = `m_g_i_o_s_${btoa(
 const Form = tcomb.form.Form
 
 class Profile extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loginFormValue: null,
+      termAgreed: false,
+      termErrorShow: false,
+      registerFormValue: null,
+    }
+  }
   onLogin() {
     const value = this.loginForm.getValue()
     if (value) {
-      this.props.loginUser(value.Username, value.Password)
+      this.props.loginUser(value.Username.toLowerCase(), value.Password)
     }
   }
 
   onRegister() {
     const value = this.registerForm.getValue()
     if (value) {
+      if (!this.state.termAgreed) {
+        this.setState({ termErrorShow: true })
+        return
+      }
+      this.setState({ termErrorShow: false })
+
       this.props.registerUser(
         value.Username,
         value.Password,
@@ -38,6 +54,8 @@ class Profile extends Component {
   }
 
   renderLoginRegisterView() {
+    const { user } = this.props
+
     return (
       <SafeAreaView
         style={{
@@ -64,12 +82,14 @@ class Profile extends Component {
             Login to share your bookmarks between your devices:
           </Text>
           <Form
+            value={this.state.loginFormValue}
+            onChange={(value) => this.setState({ loginFormValue: value })}
             style={{
               width: '90%',
             }}
             ref={c => { this.loginForm = c }}
             type={tcomb.struct({
-              Username: tcomb.subtype(tcomb.String, (t) => /^[a-z0-9_]{3,25}$/.test(t)),
+              Username: tcomb.subtype(tcomb.String, (t) => /^.{3,25}$/.test(t)),
               Password: tcomb.subtype(tcomb.String, (t) => /^.{8,400}$/.test(t)),
             })}
             options={{
@@ -86,6 +106,10 @@ class Profile extends Component {
               },
             }}
           />
+          {user.loginError && user.loginError.message === '401' &&
+            <Text style={{ color: 'red' }} >
+              Please check your username and password and try again.
+            </Text>}
           <Button
             title="Login"
             onPress={() => this.onLogin()}
@@ -100,6 +124,8 @@ class Profile extends Component {
           </Text>
 
           <Form
+            value={this.state.registerFormValue}
+            onChange={(value) => this.setState({ registerFormValue: value })}
             style={{
               width: '90%',
             }}
@@ -131,6 +157,34 @@ class Profile extends Component {
               },
             }}
           />
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={{ marginTop: 5 }}> I agree to </Text>
+            <Text
+              style={{ marginTop: 5, color: 'blue' }}
+              onPress={() => Linking.openURL('https://www.stylifier.com/policy')}
+            >
+              Terms and Conditions
+            </Text>
+            <Switch
+              style={{ marginLeft: 'auto' }}
+              value={this.state.termAgreed}
+              onChange={() => this.setState({
+                termAgreed: !this.state.termAgreed,
+                termErrorShow: this.state.termAgreed,
+              })}
+            />
+          </View>
+          {user.registeringError && user.registeringError.message === '403' &&
+            <Text style={{ color: 'red' }} >
+              Your username is taken, please try another username.
+            </Text>}
+          {this.state.termErrorShow &&
+            <Text style={{ color: 'red' }} >
+              Please agree to terms and condictions in order to register
+            </Text>}
+          <View
+            style={{ height: 20 }}
+          />
           <Button
             title="Register"
             onPress={() => this.onRegister()}
@@ -145,6 +199,22 @@ class Profile extends Component {
 
   render() {
     const { user } = this.props
+
+    if (user.loggingIn || user.registering) {
+      return (
+        <SafeAreaView
+          style={{
+            justifyContent: 'center',
+            marginTop: 50,
+            padding: 20,
+            width: '90%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+        >
+          <ActivityIndicator size="small" color="#3b4e68" />
+        </SafeAreaView>)
+    }
 
     if (user.username === deviceNameSafe) {
       return this.renderLoginRegisterView()
@@ -166,7 +236,15 @@ class Profile extends Component {
         </Text>
         <Button
           title="Logout"
-          onPress={() => this.props.logoutUser()}
+          onPress={() => {
+            this.setState({
+              loginFormValue: null,
+              termAgreed: false,
+              registerFormValue: null,
+              termErrorShow: false,
+            })
+            this.props.logoutUser()
+          }}
         />
       </SafeAreaView>
     )
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,7 +1,25 @@
 const user = (state = {}, action) => {
   switch (action.type) {
     case 'USER_INITIATED':
-      return { ...action.payload }
+      return {
+        ...action.payload,
+        loggingIn: false,
+        loginError: undefined,
+        registering: false,
+        registeringError: undefined,
+      }
+
+    case 'LOGGING_IN':
+      return { ...state, loggingIn: true, loginError: undefined }
+
+    case 'LOGIN_FAILED':
+      return { ...state, loggingIn: undefined, loginError: action.payload }
+
+    case 'REGISTERING_USER':
+      return { ...state, registering: true, registeringError: undefined }
+
+    case 'USER_REGISTRATION_FAILED':
+      return { ...state, registering: false, registeringError: action.payload }
 
     default:
       break
